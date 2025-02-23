eb89f5164f3838aded76b0b54bfe2266f032392b
devendhra <devad4828@gmail.com>
2025-02-23 20:35:38 +0530

    modified Portfolio

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./index.css";
-// import About from "./routes/About"
 import Home from "./routes/Home"
-// import Project from "./routes/Project"
-// import Skills from "./routes/Skills"
-// import Contact from "./routes/Contact"
 import { Routes,Route } from "react-router-dom";
 
 
@@ -13,10 +9,6 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<Home />}  />
-      {/* <Route path="/project" element={<Project />}  />
-      <Route path="/About" element={<About />}  />
-      <Route path="/contact" element={<Contact />}  />
-      <Route path="/skills" element={<Skills />}  /> */}
     </Routes>
     </>
   );
